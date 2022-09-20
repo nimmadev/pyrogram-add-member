3dc0c8cbb123809f666e9543ad1895b555ade663
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-09-20 15:50:12 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -13,9 +13,8 @@ async def main():
      path_group = 'data/source_user.json'
      path_group2 = 'data/target_user.json'
      path_group4 = 'data/source_admin.json'
-     path_group3 = 'data/user.json'
      await get_data(gp_s_id, gp_t_id, config)
-     filterus(path_group,path_group2,path_group3, path_group4)
+     filterus(path_group,path_group2, path_group4)
      
 
             
