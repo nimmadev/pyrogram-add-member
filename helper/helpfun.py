58bc0cde0724f6a6c2cfeca5f569e13e2b19fc45
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-10-02 14:49:32 +0530

    Update helpfun.py

--- helper/helpfun.py
+++ helper/helpfun.py
@@ -297,4 +297,8 @@ async def add_mem(user_id, config, active, method):
                 print(skipped, " : members were skipped")
                 print(privacy, " : members had privacy enable or not in mutual contact")
                 await asyncio.sleep(7000)
-                
+     else:
+          print(added, " : members were added")
+          print(skipped, " : members were skipped")
+          print(privacy, " : members had privacy enable or not in mutual contact")
+               
