3bb1023509bcb9a34f6a7677f56b040c05f3e70d
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-10-17 18:45:36 +0000

    Update helpfun.py

--- helper/helpfun.py
+++ helper/helpfun.py
@@ -80,6 +80,7 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
         print('edjt ur current count file to 0 if exist')
     for account in config['accounts']:
         phone = account["phone"]
+        print(phone, 'getting source user data')
         async with Client(phone, workdir="session") as app: 
             if await app.get_me():
                 print(phone, "is logined")
@@ -97,7 +98,7 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
                 print("%s has not joined target chat or RUN get_data.py" % phone)
                 await asyncio.sleep(1)
                 continue
-            mem=[] 
+            mem=[]
             async for member in app.get_chat_members(chat_id=gp_s_id):
                 await asyncio.sleep(.0025)
                 try:
@@ -108,54 +109,73 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
               		  "name": str(member.user.first_name),
                       "bot": member.user.is_bot,
                       "username": str(member.user.username)
-               			        }
+                    }
+                    mem.append(memb)
                 except:
                     print('error')
-                mem.append(memb)
-            print(phone, 'getting source user data')
-            mem2=[] 
-            async for member in app.get_chat_members(chat_id=gp_t_id):
-                await asyncio.sleep(.0025)
-                try:
+        if "username" == stop:
+            break
+    phonedata = config["accounts"][0]
+    phone = phonedata["phone"]
+    async with Client(phone, workdir="session") as app: 
+        if await app.get_me():
+            print(phone, "is logined")
+        else:
+            print(phone, "login failed")
+        try:
+            await app.get_chat(gp_s_id)
+        except ValueError:
+            print("%s has not joined source chat or RUN get_data.py" % phone)
+            await asyncio.sleep(1)
+        try:
+            await app.get_chat(gp_t_id)
+        except ValueError:
+            print("%s has not joined target chat or RUN get_data.py" % phone)
+            await asyncio.sleep(1)
+        mem2=[] 
+        async for member in app.get_chat_members(chat_id=gp_t_id):
+            await asyncio.sleep(.0025)
+            try:
                     #scrap member
-                    memb = {
-               		  "userid": str(member.user.id),
-              		  "status": str(member.user.status),
-              		  "name": str(member.user.first_name),
-                      "bot": member.user.is_bot,
-                      "username": str(member.user.username)
-                      
-               			        }
-                except:
-                    print('error')
+                memb = {
+             	  "userid": str(member.user.id),
+              	  "status": str(member.user.status),
+              	  "name": str(member.user.first_name),
+                  "bot": member.user.is_bot,
+                  "username": str(member.user.username)
+          	    }
+          	    
                 mem2.append(memb)
-            print(phone, 'getting target user data')
-            mem3=[]
-            async for member in app.get_chat_members(chat_id=gp_s_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-                try:
+            except:
+                print('error')
+                
+        print(phone, 'getting target user data')
+        mem3=[]
+        async for member in app.get_chat_members(chat_id=gp_s_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            try:
                     #scrap member
-                    memb = {
+                memb = {
                		"userid": str(member.user.id),
                		"name": str(member.user.first_name),
                     "bot": member.user.is_bot,
                     "username": str(member.user.username)
-               			        }
-                except:
-                    print('error')
-                mem3.append(memb)
-            print(phone, 'getting admin user data')
-            with open('data/source_user.json', 'w', encoding='utf-8') as f:
-                json.dump(mem, f, indent=4, ensure_ascii=False)
-                print("saving source user")
-            with open('data/target_user.json', 'w', encoding='utf-8') as f:
-                json.dump(mem2, f, indent=4, ensure_ascii=False)
-                print("saving target user")
-            with open('data/source_admin.json', 'w', encoding='utf-8') as f:
-                json.dump(mem3, f, indent=4, ensure_ascii=False)
-                print("saving admin user")
+                }
                 
-        if "username" == stop:
-            break
+                mem3.append(memb)
+            except:
+                print('error')
+                
+        print(phone, 'getting admin user data')
+        with open('data/source_user.json', 'w', encoding='utf-8') as f:
+            json.dump(mem, f, indent=4, ensure_ascii=False)
+            print("saving source user")
+        with open('data/target_user.json', 'w', encoding='utf-8') as f:
+            json.dump(mem2, f, indent=4, ensure_ascii=False)
+            print("saving target user")
+        with open('data/source_admin.json', 'w', encoding='utf-8') as f:
+            json.dump(mem3, f, indent=4, ensure_ascii=False)
+            print("saving admin user")
+                
             # refresh hash acces for all accounts 
  
                
@@ -387,4 +407,3 @@ async def add_mem(user_id, config, active, method):
 
     else:
         printfinal()
-    
