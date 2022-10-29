eb7bf1fd45939df8b77dee27ed999c2bad588021
nimmadev <116873964+nimmadev@users.noreply.github.com>
2022-10-29 12:12:07 +0000

    Update path

--- get_data.py
+++ get_data.py
@@ -2,19 +2,21 @@ import asyncio
 import json, os
 from pyrogram import Client, enums
 from helper.helpfun import filterus, get_data
+from pathlib import Path
 #loads config
 
 #workdir = 'session/'
 stop = input('choose method username or id: ').lower()
 async def main():
+     root = Path.cwd()
      config = (json.load(open("config.json")))
      gp_s_id = int(str("-100")+str(config['group_source']))
      gp_t_id = int(str("-100")+str(config['group_target']))
-     path_group = 'data/source_user.json'
-     path_group2 = 'data/target_user.json'
-     path_group4 = 'data/source_admin.json'
+     path_group =  root / 'data' / 'source_user.json'
+     path_group2 = root / 'data'/ 'target_user.json'
+     path_group4 = root / 'data/ 'source_admin.json'
      await get_data(gp_s_id, gp_t_id, config, stop)
-     filterus(path_group,path_group2, path_group4)
+     filterus(path_group,path_group2, path_group4, root)
      
 
             
