0c4b849eb9c15fa8bd025e5d85eba6cb0f0b663e
nimmadev <116873964+nimmadev@users.noreply.github.com>
2022-10-31 17:25:06 +0000

    Fix typo on line 16,17

--- get_data.py
+++ get_data.py
@@ -13,8 +13,8 @@ async def main():
      gp_s_id = int(str("-100")+str(config['group_source']))
      gp_t_id = int(str("-100")+str(config['group_target']))
      path_group =  root / 'data' / 'source_user.json'
-     path_group2 = root / 'data'/ 'target_user.json'
-     path_group4 = root / 'data/ 'source_admin.json'
+     path_group2 = root / 'data' / 'target_user.json'
+     path_group4 = root / 'data' / 'source_admin.json'
      await get_data(gp_s_id, gp_t_id, config, stop)
      filterus(path_group,path_group2, path_group4, root)
      
