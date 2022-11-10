b041c7dffa36318ab16652ace6eb0cf91c94d540
nimmadev <116873964+nimmadev@users.noreply.github.com>
2022-11-10 05:25:05 +0000

    Made it faster

--- helper/data.py
+++ helper/data.py
@@ -1,22 +1,26 @@
 import asyncio
-import json, os
-from pyrogram import Client, enums 
+import json
+import os
+import gc
+from pyrogram import Client, enums
 from pyrogram.errors import YouBlockedUser, RPCError, FloodWait, ChatAdminRequired, PeerFlood, PeerIdInvalid, UserIdInvalid, UserPrivacyRestricted, UserRestricted, ChannelPrivate, UserNotMutualContact, PhoneNumberBanned, UserChannelsTooMuch, UserKicked
 from pathlib import Path
 from datetime import datetime, timedelta
 import logging
+
+
 async def get_data(gp_s_id, gp_t_id, config, stop):
     try:
         count = 0
         with open('current_count.txt', 'w') as g:
             g.write(str(count))
             g.close()
-    except:
+    except BaseException:
         pass
     for account in config['accounts']:
         phone = account["phone"]
         print(phone, 'getting source user data')
-        async with Client(phone, workdir="session") as app: 
+        async with Client(phone, workdir="session") as app:
             if await app.get_me():
                 print(phone, "is logined")
             else:
@@ -24,35 +28,43 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
             try:
                 await app.get_chat(gp_s_id)
             except ValueError:
-                print("%s has not joined source chat or RUN get_data.py" % phone)
+                print(
+                    "%s has not joined source chat or RUN get_data.py" %
+                    phone)
                 await asyncio.sleep(1)
                 continue
             try:
                 await app.get_chat(gp_t_id)
             except ValueError:
-                print("%s has not joined target chat or RUN get_data.py" % phone)
+                print(
+                    "%s has not joined target chat or RUN get_data.py" %
+                    phone)
                 await asyncio.sleep(1)
                 continue
-            mem=[]
+            mem = []
             async for member in app.get_chat_members(chat_id=gp_s_id):
                 await asyncio.sleep(.0025)
+                gc.disable()
                 try:
-                    #scrap member
+                    # scrap member
                     memb = {
-               		  "userid": str(member.user.id),
-              		  "status": str(member.user.status),
-              		  "name": str(member.user.first_name),
-                      "bot": member.user.is_bot,
-                      "username": str(member.user.username)
+                        "userid": str(member.user.id),
+                        "status": str(member.user.status),
+                        "name": str(member.user.first_name),
+                        "bot": member.user.is_bot,
+                        "username": str(member.user.username)
                     }
+                    gc.disable()
                     mem.append(memb)
-                except:
+                    gc.enable()
+                except BaseException:
                     print('error')
+
         if "username" == stop:
             break
     phonedata = config["accounts"][0]
     phone = phonedata["phone"]
-    async with Client(phone, workdir="session") as app: 
+    async with Client(phone, workdir="session") as app:
         if await app.get_me():
             print(phone, "is logined")
         else:
@@ -67,39 +79,43 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
         except ValueError:
             print("%s has not joined target chat or RUN get_data.py" % phone)
             await asyncio.sleep(1)
-        mem2=[] 
+        mem2 = []
         async for member in app.get_chat_members(chat_id=gp_t_id):
             await asyncio.sleep(.0025)
+
             try:
-                    #scrap member
+                # scrap member
                 memb = {
-             	  "userid": str(member.user.id),
-              	  "status": str(member.user.status),
-              	  "name": str(member.user.first_name),
-                  "bot": member.user.is_bot,
-                  "username": str(member.user.username)
-          	    }
-          	    
-                mem2.append(memb)
-            except:
-                print('error')
-                
-        print(phone, 'getting target user data')
-        mem3=[]
-        async for member in app.get_chat_members(chat_id=gp_s_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-            try:
-                    #scrap member
-                memb = {
-               		"userid": str(member.user.id),
-               		"name": str(member.user.first_name),
+                    "userid": str(member.user.id),
+                    "status": str(member.user.status),
+                    "name": str(member.user.first_name),
                     "bot": member.user.is_bot,
                     "username": str(member.user.username)
                 }
-                
-                mem3.append(memb)
-            except:
+
+                gc.disable()
+                mem2.append(memb)
+                gc.enable()
+            except BaseException:
                 print('error')
-                
+
+        print(phone, 'getting target user data')
+        mem3 = []
+        async for member in app.get_chat_members(chat_id=gp_s_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            try:
+                # scrap member
+                memb = {
+                    "userid": str(member.user.id),
+                    "name": str(member.user.first_name),
+                    "bot": member.user.is_bot,
+                    "username": str(member.user.username)
+                }
+                gc.disable()
+                mem3.append(memb)
+                gc.enable()
+            except BaseException:
+                print('error')
+
         print(phone, 'getting admin user data')
         with open('data/source_user.json', 'w', encoding='utf-8') as f:
             json.dump(mem, f, indent=4, ensure_ascii=False)
@@ -110,6 +126,5 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
         with open('data/source_admin.json', 'w', encoding='utf-8') as f:
             json.dump(mem3, f, indent=4, ensure_ascii=False)
             print("saving admin user")
-                
-            # refresh hash acces for all accounts 
- 
+
+            # refresh hash acces for all accounts
