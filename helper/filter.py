d19947511a38d8a10c1ec139b0acd42b626b7e58
nimmadev <116873964+nimmadev@users.noreply.github.com>
2022-11-10 05:24:26 +0000

    Made it faster

--- helper/filter.py
+++ helper/filter.py
@@ -10,11 +10,10 @@ def filterus(p1,p2,p4, root_path):
                 with open(p2) as b:
                     json22 = json.loads(b.read())
 
-                for x in json22:
-                    if x in json11:
-                        json11.remove(x)
+                garbage_id = {d['userid'] for d in json22}
+                json1 = [item for item in json11 if item['userid'] not in garbage_id]
                 with open(p3, 'w', encoding='utf-8') as file:
-                    json.dump(json11, file, ensure_ascii=False, indent=4)
+                    json.dump(json1, file, ensure_ascii=False, indent=4)
                 print("Filter process done")
             except:
                 print("failed to make filter json")
@@ -25,11 +24,10 @@ def filterus(p1,p2,p4, root_path):
                     json33 = json.loads(c.read())
                 with open(p4) as h:
                     json44 = json.loads(h.read())
-                for x in json44:
-                    if x in json33:
-                        json33.remove(x)
+                garbage_i = {d['userid'] for d in json44}
+                json2 = [item for item in json33 if item['userid'] not in garbage_i]
                 with open(p3, "w", encoding='utf-8') as f:
-                    json.dump(json33, f, ensure_ascii=False, indent=4)
+                    json.dump(json2, f, ensure_ascii=False, indent=4)
             except:
                 print("no admin in group")
             #disconect
