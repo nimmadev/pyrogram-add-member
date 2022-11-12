6441bb77d52811864a4aeb069a5217019ff56097
nimmadev <116873964+nimmadev@users.noreply.github.com>
2022-11-12 13:29:12 +0000

    Fix naming

--- login.py
+++ login.py
@@ -12,5 +12,5 @@ async def loginall():
         api_id = int(account['api_id'])
         api_hash = account['api_hash']
         print(phone)
-        await login(phone, api_id, api_hash, auto_join,group_target_id, group_source_id)
+        await login(phone, api_id, api_hash, auto_join, group_source_id,  group_target_id
 asyncio.run(loginall())
