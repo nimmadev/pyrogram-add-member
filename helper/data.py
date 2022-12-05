11d9fbf27cef348f45026e7d9f62f440a9c70de2
nimmadev <nimma.singh@outlook.com>
2022-12-05 12:46:01 +0530

    update the get data

--- helper/data.py
+++ helper/data.py
@@ -15,48 +15,6 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
             g.close()
     except BaseException:
         pass
-    for account in config['accounts']:
-        phone = account["phone"]
-        PAM.info(f'{phone} getting source user data')
-        async with Client(phone, workdir="session") as app:
-            if await app.get_me():
-                PAM.info(f"{phone} is logined")
-            else:
-                PAM.info(f"{phone} login failed")
-            try:
-                await app.get_chat(gp_s_id)
-            except ValueError:
-                PAM.info(f"{phone} has not joined source chat or RUN get_data.py")
-                await asyncio.sleep(1)
-                continue
-            try:
-                await app.get_chat(gp_t_id)
-            except ValueError:
-                PAM.info(f"{phone}' has not joined target chat or RUN get_data.py")
-                await asyncio.sleep(1)
-                continue
-            mem = []
-            async for member in app.get_chat_members(chat_id=gp_s_id):
-                await asyncio.sleep(.0025)
-                gc.disable()
-                try:
-                    # scrap member
-                    memb = {
-                        "userid": str(member.user.id),
-                        "status": str(member.user.status),
-                        "name": str(member.user.first_name),
-                        "bot": member.user.is_bot,
-                        "username": str(member.user.username)
-                    }
-                    gc.disable()
-                    mem.append(memb)
-                    gc.enable()
-                except BaseException:
-                    PAM.info('error')
-            await app.stop(block=True)
-
-        if "u" == stop[0]:
-            break
     phonedata = config["accounts"][0]
     phone = phonedata["phone"]
     async with Client(phone, workdir="session") as app:
@@ -110,7 +68,7 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
                 gc.enable()
             except BaseException:
                 PAM.info('error')
-
+        app.stop()
         PAM.info(f'{phone} getting admin user data')
         with open('data/source_user.json', 'w', encoding='utf-8') as f:
             json.dump(mem, f, indent=4, ensure_ascii=False)
@@ -121,7 +79,48 @@ async def get_data(gp_s_id, gp_t_id, config, stop):
         with open('data/source_admin.json', 'w', encoding='utf-8') as f:
             json.dump(mem3, f, indent=4, ensure_ascii=False)
             PAM.info("saving admin user")
+        
         if "u" == stop[0]:
             exit()
+    total_account = len(config['accounts'])
+    account = config['accounts']
+    for numb in range(1, total_account+1):
+        phone = account[numb]["phone"]
+        PAM.info(f'{phone} getting source user data')
+        async with Client(phone, workdir="session") as app:
+            if await app.get_me():
+                PAM.info(f"{phone} is logined")
+            else:
+                PAM.info(f"{phone} login failed")
+            try:
+                await app.get_chat(gp_s_id)
+            except ValueError:
+                PAM.info(f"{phone} has not joined source chat or RUN get_data.py")
+                await asyncio.sleep(1)
+                continue
+            try:
+                await app.get_chat(gp_t_id)
+            except ValueError:
+                PAM.info(f"{phone}' has not joined target chat or RUN get_data.py")
+                await asyncio.sleep(1)
+                continue
+            mem = []
+            async for member in app.get_chat_members(chat_id=gp_s_id):
+                await asyncio.sleep(.0025)
+                gc.disable()
+                try:
+                    # scrap member
+                    memb = {
+                        "userid": str(member.user.id),
+                        "status": str(member.user.status),
+                        "name": str(member.user.first_name),
+                        "bot": member.user.is_bot,
+                        "username": str(member.user.username)
+                    }
+                    gc.disable()
+                    mem.append(memb)
+                    gc.enable()
+                except BaseException:
+                    PAM.info('error')
 
             # refresh hash acces for all accounts
