15df6787061a5119946c64c70bcc8f65717c4c17
nimmadev <nimma.singh@outlook.com>
2022-12-07 12:34:47 +0530

    add leave chat

---
+++ leave_group.py
@@ -0,0 +1,24 @@
+import asyncio
+import json
+from helper.leave_group import leave
+from helper.pam_log import pamlog
+#load config for accounts
+config=json.load(open('config.json'))
+group_source_id= '-100 ' + str(config['group_source'])
+group_target_id= '-100' + str(config['group_target'])
+option = input('type 1 to leave source group and 2 for both: ')
+async def loginall():
+    PAM = pamlog('PAM-Leave_Group')
+    PAM.propagate = False
+    for account in config['accounts']:
+        phone = account['phone']
+        api_id = int(account['api_id'])
+        api_hash = account['api_hash']
+        PAM.info(phone)
+        if option == '1':
+            await leave(phone, api_id, api_hash, group_source_id)
+        elif option == '2':
+            await leave(phone, api_id, api_hash, group_source_id,  group_target_id) 
+        else:
+            PAM.info('Wrong option')
+asyncio.run(loginall())
