299ce1066decdbd6e330acd9856a7c1a11078d40
nimmadev <116873964+nimmadev@users.noreply.github.com>
2022-12-10 03:04:20 +0530

    Update leave_group.py

--- helper/leave_group.py
+++ helper/leave_group.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from datetime import datetime, timedelta
 import logging
 from helper.pam_log import pamlog
-async def leave(phone, api_id, api_hash, group_source_id,  group_target_id=None):
+async def leave(phone, api_id, api_hash, group_source_id):
     # create logger
     PAM = pamlog('PAM-Login-Signup')
     PAM.propagate = False
@@ -17,11 +17,25 @@ async def leave(phone, api_id, api_hash, group_source_id,  group_target_id=None)
                 await app.leave_chat(group_source_id)
             except BaseException as e:
                     PAM.info("could not Leave source group")
-            if group_target_id:
-                try:
-                    await app.leave_chat(group_target_id)
-                except BaseException as e:
-                    PAM.info("could not leave target group")
             await asyncio.sleep(.1)
         else:
             PAM.info(phone, 'login failed')
+async def leave2(phone, api_id, api_hash, group_source_id,  group_target_id):
+    # create logger
+    PAM = pamlog('PAM-Login-Signup')
+    PAM.propagate = False
+    async with Client(phone, api_id, api_hash, workdir='session')as app:
+        if await app.get_me():
+            PAM.info(f'{phone} is logined')
+            try:
+                await app.leave_chat(group_source_id)
+            except BaseException as e:
+                    PAM.info("could not Leave source group")
+            try:
+                await app.leave_chat(group_target_id)
+            except BaseException as e:
+                PAM.info("could not leave target group")
+            await asyncio.sleep(.1)
+        else:
+            PAM.info(phone, 'login failed')
+
