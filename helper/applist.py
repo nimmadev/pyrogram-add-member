5efda146d3b60ee4c7b4659670f966dcfd55a058
nimmadev <116873964+nimmadev@users.noreply.github.com>
2022-12-17 17:33:03 +0530

    Update applist.py

--- helper/applist.py
+++ helper/applist.py
@@ -1,30 +1,37 @@
 import asyncio
-import json, os
-from pyrogram import Client, enums 
+import json
+import os
+from pyrogram import Client, enums
 from pyrogram.errors import YouBlockedUser, UserDeactivatedBan, FloodWait, ChatAdminRequired, PeerFlood, PeerIdInvalid, UserIdInvalid, UserPrivacyRestricted, UserRestricted, ChannelPrivate, UserNotMutualContact, PhoneNumberBanned, UserChannelsTooMuch, UserKicked
 from pathlib import Path
 from datetime import datetime, timedelta
 import logging
 from helper.pam_log import pamlog
+
+
 async def addlogin(config):
     # create logger
-    PAM= pamlog('PAM-LOGIN')
+    PAM = pamlog('PAM-LOGIN')
     PAM.propagate = False
-    #login starthere
+    # login starthere
     applist = []
     for account in config:
         phone = account["phone"]
-        app = Client(phone,api_id=account["api_id"], api_hash=account["api_hash"], workdir="session")
+        app = Client(
+            phone,
+            api_id=account["api_id"],
+            api_hash=account["api_hash"],
+            workdir="session")
         try:
             await app.start()
         except UserDeactivatedBan:
             PAM.info(f"{phone} has been removed from telegram")
-	except Execption as e :
-	    PAM.info(f"{e} Share this error to @nimmadev on telegram") 
-        check = await app.get_me() 
-        try:   
+        except Execption as e:
+            PAM.info(f"{e} Share this error to @nimmadev on telegram")
+        check = await app.get_me()
+        try:
             spam = config["spam_check"]
-        except:
+        except BaseException:
             spam = False
         if check:
             PAM.info(f'{phone} login sucess')
@@ -33,11 +40,13 @@ async def addlogin(config):
             if spam:
                 try:
                     messegespam = await app.send_message('@spambot', '/start')
-                    messget = await  app.get_messages('@spambot', message_ids=(int(messegespam.id) + 1)).text
-                    listofnum =["1","2","3","4","5","6","7","8","9","0"]
-                    checktext = [x for x in listofnum if(x in messget)] 
+                    messget = await app.get_messages('@spambot', message_ids=(int(messegespam.id) + 1)).text
+                    listofnum = [
+                        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+                    checktext = [x for x in listofnum if (x in messget)]
                     if checktext:
-                        PAM.info(f'{phone} is limited or disabled! will no be used for this RUN')
+                        PAM.info(
+                            f'{phone} is limited or disabled! will no be used for this RUN')
                     else:
                         applist.append({'phone': account["phone"], 'app': app})
                 except (BaseException, YouBlockedUser):
@@ -49,4 +58,3 @@ async def addlogin(config):
             PAM.info(f'{phone} login failed')
             await asyncio.sleep(1)
     return applist
-                
